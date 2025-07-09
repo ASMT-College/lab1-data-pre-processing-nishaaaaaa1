96a4a1455c6cfde3c65ec047538c7ea7755a6e5a
Nisha khadka <120707560+nishaaaaaa1@users.noreply.github.com>
2025-07-09 08:33:12 +0545

    Add files via upload

---
+++ lab-1/ni.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv("employee_data.csv")
+print("Initial Data:\n", df.head())
+
+df['Age'].fillna(df['Age'].mean(), inplace=True)
+df['Salary'].fillna(df['Salary'].mean(), inplace=True)
+
+df['Department'] = df['Department'].str.lower()
+df['Department'] = df['Department'].replace({
+    'human resources': 'hr',
+    'h.r.': 'hr',
+    'hr': 'hr',
+    'finance': 'finance',
+    'it': 'it'
+})
+
+df = df.drop_duplicates(subset='ID', keep='first')
+df.to_csv("cleaned_employee_data.csv", index=False)
+print("Cleaned data saved to 'cleaned_employee_data.csv'")
